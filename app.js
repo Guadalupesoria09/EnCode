a7029a9e4890bb32d96bc13b1b6b3ddbe7f18418
MasterCrafter69 <r2d2.dany@gmail.com>
2024-08-28 14:36:26 -0600

    instalar paquetes

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+//const { decifrarAES } = require('./generarXML');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.listen(procces.env.PORT || 2050);
