2447884457b723c08ae10b0e923d8e95ba20b355
Netogu1llen <pepeppolloxd12@gmail.com>
2024-08-29 15:41:21 -0600

    home page visible y fluida after login

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('view engine', 'ejs');
-app.set('views', 'views');
-
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
@@ -22,19 +20,21 @@ app.use((request, response, next) => {
     console.log('Middleware!');
     next(); //Le permite a la petición avanzar hacia el siguiente middleware
 });
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 const usuariosRoutes = require('./routes/usuarios.routes');
 app.use('/usuarios', usuariosRoutes);
 
-// const prepararRutas = require('./routes/preparar.routes');
-// app.use('/preparar', prepararRutas);
+// const adminRoutes=require('./routes/admin.routes');
+// app.use('/admin', adminRoutes);
 
-// const chilaquilesRutas = require('./routes/chilaquiles.routes');
-// app.use('/', chilaquilesRutas);
+// const duenoRoutes = require('./routes/dueno.routes');
+// app.use('/dueno', duenoRoutes);
+
 
 app.use((request, response, next) => {
-    response.statusCode = 404;
-    response.render('404');
+    response.statusCode = (404).render('404');
 })
 
 app.listen(3000);
