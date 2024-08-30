27be38adc07b6a047d71373fd8722d5f1c2fb996
Kate Rdz <a01234357@tec.mx>
2024-08-29 22:07:02 -0600

    finVistaregistrarRecompensas

--- routes/dueno.routes.js
+++ routes/dueno.routes.js
@@ -6,4 +6,10 @@ router.get('/promociones', (request, response) => {
     response.render('crearPromocion'); // Renderiza la vista 'crearPromocion.ejs'
 });
 
+// Ruta para mostrar el formulario de registrar recompensa
+router.get('/recompensas', (request, response) => {
+    response.render('registrarRecompensa'); // Renderiza la vista 'registrarRecompensa.ejs'
+});
+
+
 module.exports = router;
