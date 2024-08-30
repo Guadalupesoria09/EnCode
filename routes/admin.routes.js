0e7d55b3a7458d88e97913815c5c897a5ec3a474
MasterCrafter69 <r2d2.dany@gmail.com>
2024-08-30 07:43:55 -0600

    resolver problema ñ y routes

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/registrarSucursal');
-router.get('/registrarDueño');
+
+router.get('/registrarSucursales', (request, response) => {
+    response.render('registrarSucursales'); // Renderiza la vista 'crearPromocion.ejs'
+});
+
+router.get('/registrarDueno', (request, response) => {
+    response.render('registrarDueno'); // Renderiza la vista 'crearPromocion.ejs'
+});
+
+router.get('/sucursales', (request, response) => {
+    response.render('sucursales'); // Renderiza la vista 'crearPromocion.ejs'
+});
 
 module.exports = router;
