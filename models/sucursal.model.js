891d5e48e20130e874252911891e3b43b8b73955
MasterCrafter69 <r2d2.dany@gmail.com>
2024-09-09 19:45:51 -0600

    modelo terminado

---
+++ models/sucursal.model.js
@@ -0,0 +1,19 @@
+const db = require('../util/database');
+
+module.exports = sucursal {
+
+    constructor(miDireccion, miCP, miCiudad, miEstado, miNumSucursal, NombreSucursal {
+        this.Direccion = miDireccion;
+	this.CP = miCP;
+	this.Ciudad = miCiudad;
+	this.Estado = miEstado;
+	this.NumSucursal = miNumSucursal;
+	this.NombreSucursal = miNombreSucursal;
+    }
+
+    save(){
+        return db execute(
+        'INSERT INTO Sucursal (Direccion, CP, Ciudad, Estado, NumSucursal, NombreSucursal) VALUES(?,?,?,?,?,?)'
+	[this.Direccion, this.CP, this.Ciudad, this.Estado, this.NumSucursal, this.NombreSucursal]);
+    }
+}
