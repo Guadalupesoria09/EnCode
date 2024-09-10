8854a5da0237d432bd700307157cc58f4e1a109e
MasterCrafter69 <r2d2.dany@gmail.com>
2024-09-09 20:31:58 -0600

    conectar BD

--- models/sucursal.model.js
+++ models/sucursal.model.js
@@ -1,8 +1,8 @@
 const db = require('../util/database');
 
-module.exports = sucursal {
+module.exports = class sucursal {
 
-    constructor(miDireccion, miCP, miCiudad, miEstado, miNumSucursal, NombreSucursal {
+    constructor(miDireccion, miCP, miCiudad, miEstado, miNumSucursal, NombreSucursal) {
         this.Direccion = miDireccion;
 	this.CP = miCP;
 	this.Ciudad = miCiudad;
@@ -12,7 +12,7 @@ module.exports = sucursal {
     }
 
     save(){
-        return db execute(
+        return db.execute(
         'INSERT INTO Sucursal (Direccion, CP, Ciudad, Estado, NumSucursal, NombreSucursal) VALUES(?,?,?,?,?,?)'
 	[this.Direccion, this.CP, this.Ciudad, this.Estado, this.NumSucursal, this.NombreSucursal]);
     }
