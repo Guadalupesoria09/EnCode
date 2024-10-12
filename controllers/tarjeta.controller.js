ab3fca3c08dae7bdfca8682707495fb73d7097b6
MasterCrafter69 <r2d2.dany@gmail.com>
2024-10-12 15:48:55 -0600

    crear POST(sin terminar)

--- controllers/tarjeta.controller.js
+++ controllers/tarjeta.controller.js
@@ -1,9 +1,16 @@
 const Tarjeta = require('../models/tarjeta.model.js')
-const PromoRecomp = require('../models/promocionRecompensa.model.js')
+const Promocion = require('../models/promocionRecompensa.model.js')
 
-/*
-Renderiza la página para editar la tarjeta con las promociones disponibles.
-*/
+//Controlador para cargar la página de editar el formato de la tarjeta 
+exports.get_editorTarjeta = (request, response, next) => {
+    response.render('editorTarjeta', {
+            username: request.session.NombreUsuario || '', 
+            csrfToken: request.csrfToken(),
+    });
+  
+};
+
+//Controlador para cargar la pagina de visualizar y editar los parametros de la  tarjeta.
 exports.get_tarjeta = (request, response, next) => {
     PromoRecomp.fetchAll()
         .then(([promociones, fieldData]) => {
@@ -19,11 +26,22 @@ exports.get_tarjeta = (request, response, next) => {
         });
 };
 
-exports.get_editorTarjeta = (request, response, next) => {
-    response.render('editorTarjeta', {
-            username: request.session.NombreUsuario || '', 
-            csrfToken: request.csrfToken(),
+//Controlador para manejar la solicitud POST al registrar nuevos parametros a la tarjeta
+exports.post_tarjeta = (request, response, next) => {
+    const tarjeta = new Tarjeta(
+        request.body.Limite,
+	request.body.Vigencia,
+    );
+
+    tarjeta.save()
+    .then(() => {
+       request.session.mensaje = 'Parametros actualizados';  // Mensaje de éxito.
+       response.redirect('editarTarjeta');
+    })
+    .catch((error) => {
+        console.log(error);  // En caso de error.
+        request.session.mensaje = 'No se pudieron guardar los parametros';  // Mensaje de error.
+        response.redirect('editarTarjeta');  // Redirige de vuelta a la página de editar tarjeta.
     });
-  
 };
 
